e9e57b434e2495b25a483e6030b011d9fd7c0b65
abdessamadsouilem <abdessamadsouilem1@gmail.com>
2024-05-01 19:20:47 +0100

    complete registration api

--- Makefile
+++ Makefile
@@ -4,3 +4,9 @@ test:
 	@go test -v ./...
 run: build
 	@./go_api
+migration:
+	@migrate create -ext sql -dir cmd/migrate/migrations $(filter-out $@,$(MAKECMDGOALS))
+migrate-up:
+	@go run cmd/migrate/main.go up
+migrate-down:
+	@go run cmd/migrate/main.go down
--- config/env.go
+++ config/env.go
@@ -25,7 +25,7 @@ func initConfig() Config {
 		Port:       getEnv("PORT", "8080"),
 		DBUser:     getEnv("DB_USER", "root"),
 		DBPasswd:   getEnv("DB_PASSWORD", ""),
-		DBAddr:     fmt.Sprintf("%s:%s", getEnv("DB_HOST", "127.0.0.1"), getEnv("DB_PORT", "3306")),
+		DBAddr:     fmt.Sprintf("%s:%s", getEnv("DB_HOST", "127.0.0.1"), getEnv("DB_PORT", "3307")),
 		DBName:     getEnv("DB_NAME", "ecom"),
 	}
 }
